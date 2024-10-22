1249ec66b028c93a4ac7d1f5ab19300024fc463a
Mario09122004 <169079274+Mario09122004@users.noreply.github.com>
2024-10-22 14:34:15 -0600

    Avance

--- Integradora/app.py
+++ Integradora/app.py
@@ -4,8 +4,16 @@ app = Flask(__name__)
 
 #Funciones a llamar desde la web despues de cargar la pagina
 @app.route('/api/contactos')
-def mi_funcion():
+def contactos():
     return Response("<h1>Se esta tranajando en una funcion que imprima los contactos de la base de datos :)<h1>", mimetype='text/html')
+
+@app.route('/api/texto_vision_mision')
+def texto_mision_vision():
+    return Response("<h1>Llamar los datos de contenido para plazmrlos en un lugar, proximamente<h1>", mimetype='text/html')
+
+@app.route('/api/texto_valores')
+def texto_valores():
+    return Response("<h1>Llamar los valres y derechos de la empresa<h1>", mimetype='text/html')
 
 #Direccionamiento de la web
 @app.route('/')
