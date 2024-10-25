f303617ef23b94333d4c0bf1da5e3c3bceacb7a5
Mario09122004 <169079274+Mario09122004@users.noreply.github.com>
2024-10-24 19:17:51 -0600

    Avance 4

--- Integradora/app.py
+++ Integradora/app.py
@@ -1,97 +1,152 @@
 from flask import Flask, render_template, Response
-import mysql.connector 
+from sqlalchemy import create_engine, text
 
 app = Flask(__name__)
 
-# Funciones de bases de datos
-def connect_db():
-    connection = mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="",
-        database="integradora"
-    )
-    return connection
+#Conexión
+engine = None
 
+#Función para iniciar la base de datos
+def init_db():
+    global engine
+    if engine is None:
+        engine = create_engine('mysql+pymysql://root:@localhost/integradora')
+        #mysql+pymysql://<usuario>:<contraseña>@<host>/<nombre_base_de_datos>
 
-#Funciones a llamar desde la web despues de cargar la pagina
+#Funciones a llamar desde la web después de cargar la página
 @app.route('/api/contactos')
 def contactos():
-    connection = connect_db() 
-    cursor = connection.cursor()
-    cursor.execute("SELECT contacts.Facebook, contacts.Instagram, contacts.Tik_tok, contacts.Email, contacts.Twitter, contacts.Whatsapp, contacts.Phone FROM contacts;")
-    contactos = cursor.fetchall()
-    nombre_columnas = [desc[0] for desc in cursor.description]
-    html = ""
-    
-    for redes in contactos:
-        html+='<div class="contacto_contenedor">'
-        for url, nombre in zip(redes,nombre_columnas):
-            if not url=="":
-                html += f"""
-                <div class="elementos">
-                    <a href="{url}">
-                        <img src="static/image/{nombre}.svg" alt="{nombre}" class="{nombre}">
-                    </a>
-                </div>
-                """
-        html+='</div><hr>'
+    try:
+        init_db()
+        with engine.connect() as connection:
+            result = connection.execute(text("SELECT contacts.Facebook, contacts.Instagram, contacts.Tik_tok, contacts.Email, contacts.Twitter, contacts.Whatsapp, contacts.Phone FROM contacts;"))
+            contactos = result.fetchall()
+            nombre_columnas = result.keys()
+            html = ""
 
-    cursor.close()
-    connection.close()
-    
-    return Response(html, mimetype='text/html')
+            for redes in contactos:
+                html += '<div class="contacto_contenedor">'
+                for url, nombre in zip(redes, nombre_columnas):
+                    if url:
+                        html += f"""
+                        <div class="elementos">
+                            <a href="{url}">
+                                <img src="static/image/{nombre}.svg" alt="{nombre}" class="{nombre}">
+                            </a>
+                        </div>
+                        """
+                html += '</div><hr>'
+
+            return Response(html, mimetype='text/html')
+    except:
+        return Response("Error 404", mimetype='text/html')
 
 @app.route('/api/texto_vision_mision')
 def texto_mision_vision():
-    connection = connect_db() 
-    cursor = connection.cursor()
-    cursor.execute('SELECT content.Title, content.Describe FROM content WHERE content.Title!="Valores" AND content.Title!="Derechos";')
-    contenido = cursor.fetchall()
-    html = ""
-    
-    for titulo,descripcion in contenido:
-        html+='<div class="valores_derechos">'
-        html+=f"""
-            <h2>{titulo}</h2>
-            <p>{descripcion}</p> 
-        """
-        html+='</div><br>'
+    try:
+        init_db()
+        with engine.connect() as connection:
+            result = connection.execute(text('SELECT content.Title, content.Describe FROM content WHERE content.Title!="Valores" AND content.Title!="Mision" AND content.Title!="Vision";'))
+            contenido = result.fetchall()
+            html = ""
 
-    cursor.close()
-    connection.close()
-    
-    return Response(html, mimetype='text/html')
+            for titulo, descripcion in contenido:
+                html += '<div class="valores_derechos">'
+                html += f"""
+                    <h2>{titulo}</h2>
+                    <p>{descripcion}</p> 
+                """
+                html += '</div><br>'
+
+            return Response(html, mimetype='text/html')
+    except:
+        return Response("Error 404", mimetype='text/html')
 
 @app.route('/api/texto_valores')
 def texto_valores():
-    connection = connect_db() 
-    cursor = connection.cursor()
-    cursor.execute('SELECT content.Title, content.Describe FROM content WHERE content.Title="Valores" OR content.Title="Derechos";')
-    contenido = cursor.fetchall()
-    html = ""
-    
-    for titulo,descripcion in contenido:
-        html+='<div class="valores_derechos">'
-        html+=f"""
-            <h2>{titulo}</h2>
-            <p>{descripcion}</p> 
-        """
-        html+='</div><br>'
+    try:
+        init_db()
+        with engine.connect() as connection:
+            result = connection.execute(text('SELECT content.Title, content.Describe FROM content WHERE content.Title="Valores" OR content.Title="Misión" OR content.Title="Visión";'))
+            contenido = result.fetchall()
+            html = ""
 
-    cursor.close()
-    connection.close()
-    
-    return Response(html, mimetype='text/html')
+            for titulo, descripcion in contenido:
+                html += '<div class="valores_derechos">'
+                html += f"""
+                    <h2>{titulo}</h2>
+                    <p>{descripcion}</p> 
+                """
+                html += '</div><br>'
 
-#Direccionamiento de la web
+            return Response(html, mimetype='text/html')
+    except:
+        return Response("Error 404", mimetype='text/html')
+
+@app.route('/api/tabla_productos')
+def tabla_productos():
+    try:
+        init_db()
+        with engine.connect() as connection:
+            result = connection.execute(text('SELECT products.Name, products.Model, products.Size, products.Material_composition, products.Price_per_unit, products.Color, products.Id_product FROM products;'))
+            contenido = result.fetchall()
+            
+            html = """
+            <table>
+                <thead>
+                    <tr>
+                        <th>Producto</th>
+                        <th>Modelo</th>
+                        <th>Tamaño</th>
+                        <th>Material de composisión</th>
+                        <th>Precio</th>
+                        <th>Color</th>
+                        <th></th>
+                    </tr>
+                </thead>
+                <tbody>
+            """
+            for info in contenido:
+                html += f"""
+                    <tr>
+                        <td>{info[0]}</td>
+                        <td>{info[1]}</td>
+                        <td>{info[2]}</td>
+                        <td>{info[3]}</td>
+                        <td>{info[4]}</td>
+                        <td>{info[5]}</td>
+                        <td>
+                            <button onclick="editarProducto({info[6]})" class="detalles">Detalles</button>
+                            <button onclick="editarProducto({info[6]})" class="editar">Editar</button>
+                            <button onclick="eliminarProducto({info[6]})" class="eliminar">Eliminar</button>
+                        </td>
+                    </tr>
+                    
+                """
+
+            html += """
+                </tbody>
+            </table>
+            """
+            
+            return Response(html, mimetype='text/html')
+    except:
+        return Response("Error 404", mimetype='text/html')
+
+#Direccionamientos
 @app.route('/')
 def home():
     return render_template('index.html')
 
-@app.route('/otra_pagina')
-def ejemplo():
-    return render_template('test.html')
+@app.route('/administrador')
+def administrador():
+    return render_template('administracion.html')
 
+@app.route('/inicio_usuario')
+def inicio_usuario():
+    return render_template('registro_Inicio.html')
+
+# Inicio del servidor
 if __name__ == '__main__':
+    init_db()
     app.run(debug=True)
